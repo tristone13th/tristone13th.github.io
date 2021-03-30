776c74bc3a12788e40dd8389a6ef012b63fbefcf
tristone13th <1272652761@qq.com>
2021-03-30 17:37:22 +0800

    update

--- js/easybook.js
+++ js/easybook.js
@@ -285,7 +285,7 @@ function RealLoad() {
     var sc = document.createElement('script');
     sc.setAttribute('type', 'text/javascript');
     sc.setAttribute('async', 'true');
-    sc.setAttribute('src', 'https://cdnjs.cloudflare.com/ajax/libs/mathjax/2.7.1/MathJax.js?config=TeX-AMS_CHTML');
+    sc.setAttribute('src', 'https://cdn.jsdelivr.net/npm/mathjax@3/es5/tex-mml-chtml.js');
     document.body.appendChild(sc);
 }
 
